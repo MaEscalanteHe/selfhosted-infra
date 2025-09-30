58539135f6b12b51b588b4421a88645c2884c10a
Manuel Escalante <maescalantehe@gmail.com>
2025-09-29 21:53:49 -0400

    feat: enhance setup scripts for n8n and Traefik with Cloudflared by adding permissions setting for data directories

--- traefik-cloudflared/setup.sh
+++ traefik-cloudflared/setup.sh
@@ -7,6 +7,9 @@ sleep 1
 echo "Creating data directories..."
 mkdir -p data/letsencrypt data/crowdsec data/crowdsec logs/traefik
 
+echo "Setting permissions for data directories..."
+chmod 700 data/*
+
 echo "Creating acme.json file for Let's Encrypt certificates..."
 touch data/letsencrypt/acme.json
 chmod 600 data/letsencrypt/acme.json
